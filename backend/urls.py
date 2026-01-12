31fc947a518110701cf3d2371d7269b20515df44
YvannLogi <yvanndavid03@gmail.com>
2026-01-12 12:55:41 +0000

    add: create admin user for render

--- backend/urls.py
+++ backend/urls.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import get_user_model
+
+
+# add a temporary admin for deployment
+
+User = get_user_model()
+if not User.objects.filter(username='taskad').exists():
+    User.objects.create_superuser('taskas', 'task@admin.com', 'Admin12345')
+    print("ADMIN CRÉÉ SUR RENDER !")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
